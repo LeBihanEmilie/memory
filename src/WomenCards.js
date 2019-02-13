ee0859c9d55c016fac6957734252757f26f180ce
Emilie Le Bihan <emilielebihan@MacBook-Pro-de-Emilie.local>
2019-02-13 07:58:59 +0100

    modification

--- src/WomenCards.js
+++ src/WomenCards.js
@@ -14,9 +14,10 @@ class WomenCards extends Component {
       {
         name: 'Betty Davis',
         url: './img/BettyDavis.png' ,
-        visible: false,         
+        visible: false ,        
         hidden:'./img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -24,7 +25,8 @@ class WomenCards extends Component {
         url: './img/NellyBly.png',
         visible: false,
         hidden:'./img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -32,7 +34,8 @@ class WomenCards extends Component {
         url: './img/SonitaAlizadeh.png',
         visible: false,
         hidden: './img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -40,7 +43,8 @@ class WomenCards extends Component {
         url: './img/TempleGrandin.png',
         visible: false,
         hidden:'./img/background.jpg',   
-        matched: false  
+        matched: false ,
+        disabled: false  
       },
 
       {
@@ -48,7 +52,8 @@ class WomenCards extends Component {
         url: './img/BettyDavis.png',
         visible: false,
         hidden: './img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -56,7 +61,8 @@ class WomenCards extends Component {
         url: './img/NellyBly.png',
         visible: false,
         hidden: './img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -64,7 +70,8 @@ class WomenCards extends Component {
         url: './img/wuzetian.png',
         visible: false,
         hidden:'./img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -72,7 +79,8 @@ class WomenCards extends Component {
         url: './img/SonitaAlizadeh.png',
         visible: false,
         hidden: './img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
 
@@ -81,7 +89,8 @@ class WomenCards extends Component {
         url: './img/ThereseClerc.png',
         visible: false,
         hidden:'./img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -89,7 +98,8 @@ class WomenCards extends Component {
         url: './img/wuzetian.png',
         visible: false,
         hidden:'./img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -97,7 +107,8 @@ class WomenCards extends Component {
         url: './img/ThereseClerc.png',
         visible: false,
         hidden:'./img/background.jpg',
-        matched: false
+        matched: false,
+        disabled: false 
       },
 
       {
@@ -105,7 +116,8 @@ class WomenCards extends Component {
         url: './img/TempleGrandin.png',
         visible: false,
         hidden:'./img/background.jpg',
-        matched: false              
+        matched: false,
+        disabled: false             
       }
     ],
     selection: [],
@@ -113,52 +125,57 @@ class WomenCards extends Component {
 }
 // le setState ne lit pas les index d'un tableau, donc il faut déclarer une variable, et lui donner la valeur de "characters".
   
-  switchCard = (index) => {
-    let modif = this.state.characters 
-    modif[index].visible = ! this.state.characters[index].visible
-    this.setState({
-      characters: modif
-    })
-  }
+  // switchCard = (index) => {
+  //   const modif = this.state.characters 
+  //  modif[index].visible = ! this.state.characters[index].visible
+  //   this.setState({
+  //     characters: modif
+  //   })
+  // }
 
   shuffleCard = (index, url) => {
-    let shuffled = underscore.shuffle(this.state.characters) 
+    const shuffled = underscore.shuffle(this.state.characters) 
     shuffled[index, url] = ! this.state.characters[index, url]
     this.setState({
       characters: shuffled
     })
   }
 
-  selectCard = (index) => {
-   if (this.state.selection.length >= 2) {
-    this.setState({selection: [...this.state.selection, this.state.characters[index]]})
-    // console.log(this.state.selection);
-    // let selection = this.state.characters
-    //   this.setState({
-    //      characters: selection
-    //   })
-    // }
-  }
+//   selectCard = (index) => {
+//    if (this.state.selection.length == 2) {
+//     this.setState({selection: [...this.state.selection, this.state.characters[index]]})
+//     console.log(this.state.selection);
+//     let selection = this.state.characters
+//       this.setState({
+//          characters: selection
+//       })
+//     }
+//  }
+
+   toggleVisible = (index) => {
+        if(this.state.characters.length < 2){
+          this.state.characters.push(index);
+        }
+        if(this.state.selection.length == 2 && (this.state.characters[0].value == this.state.characters[1].value)){
+            this.state.characters[0].find = true
+            this.state.characters[0].find = true
+        } else {
+          this.state.characters.splice(0,2)
+        }
+      }
+    
 
 
 render() {
-  // faire une condition de départ qui dit si le tableau sélection n'est pas égal à null, alors on return qqchse
-  if (this.state.selection: ! null) {
-
-  }
-  if (this.state.selection[0].name === this.state.selection[1].name) {
-    console.log("matched")
-  } 
-
-  return( 
+   return( 
     <div className="fond"> 
       <div className="cards"> 
         {this.state.characters.map((item, index) => {
           return(
             item.visible ? 
-            <img key={index} src={process.env.PUBLIC_URL + item.url } className="characters"  onClick={this.onClick} alt="characters" />
+            <img key={index} src={process.env.PUBLIC_URL + item.url } className="characters"  onClick={() => this.toggleVisible(index)} alt="characters" />
             :
-            <img key={index} src={process.env.PUBLIC_URL + item.hidden } className= {item.visible ? "characters" : "character-hidden"}  onClick={() => { this.switchCard(index); this.selectCard(index);}} alt="characters" />  
+            <img key={index} src={process.env.PUBLIC_URL + item.hidden } className= {item.visible ? "characters" : "character-hidden"}  onClick={() => this.toggleVisible(index)} alt="characters" />  
           )}
         )}
         </div>
@@ -172,7 +189,3 @@ render() {
 export default WomenCards;
 
 
-// shuffleCard = () => {
-  //   let shuffled = underscore.shuffle(this.state.characters);
-  //   console.log(shuffled)
-  //   }
---
+++ src/idea.js
@@ -0,0 +1,81 @@
+
+  // selectCard = (index) => {
+  //  if (this.state.selection.length >= 2) {
+  //   this.setState({selection: [...this.state.selection, this.state.characters[index]]})
+    // console.log(this.state.selection);
+    // let selection = this.state.characters
+    //   this.setState({
+    //      characters: selection
+    //   })
+    // }
+ // }
+
+///////////////////////////////////////////
+
+//render() {
+    // // faire une condition de départ qui dit si le tableau sélection n'est pas égal à null, alors on return qqchse
+    // if (this.state.selection: ! null) {
+  
+    // }
+    // if (this.state.selection[0].name === this.state.selection[1].name) {
+    //   console.log("matched")
+    // } 
+
+/////////////////////////////////////////////
+
+
+    // import React, { Component } from 'react'
+
+
+    // class ComponentCard extends Component {
+    //   state = {
+    //     allCards : [{
+    //       id:'card1',
+    //       urlImage:"http://",
+    //       visible: false,
+    //       find:false,
+    //       value:'10_heart',
+    //       pairId: 'card2'
+    //     },{
+    //       id:'card2',
+    //       urlImage:"http://",
+    //       visible: false,
+    //       find:false,
+    //       value:'10_heart',
+    //       pairId: 'card1'
+    //     }],
+    //     urlImageBack: "http://",
+    //     compareArray:[]
+    //   }
+    
+    //   _toggleVisible(el){
+    //     el.visible = true;
+    //     if(this.state.compareArray.length < 2){
+    //       this.state.compareArray.push(el);
+    //     }
+    //     if(this.state.compareArray.length == 2 && (this.state.compareArray[0].value == this.state.compareArray[1].value)){
+    //         this.state.compareArray[0].find = true
+    //         this.state.compareArray[0].find = true
+    //     } else {
+    //       this.state.compareArray.splice(0,2)
+    //     }
+        
+    //   }
+    
+    
+    //   render(){
+    //       const {allCards} = this.state;
+    //     return(
+    //       {allCards.length > 0 ? (
+    //         allCards.map((el, i) => (
+    //         <div ref={i} id={el.id} onClick={() => _toggleVisible(el)}>
+    //           <img src={el.visible ? el.urlImage : urlImageBack} />
+    //         </div>    
+    //       )): <span> Vous n'avez aucune carte </span>}
+    //       )
+    //   }
+    
+      
+    // }
+
+
